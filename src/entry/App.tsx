b1b29972227abac4c188384dec79b345ecd77e01
Ryan <ryanyeojl@gmail.com>
2025-05-25 00:01:28 +0800

    down with the monarchy

--- src/entry/App.tsx
+++ src/entry/App.tsx
@@ -1,26 +1,26 @@
 import Navbar from "@/views/layout/Navbar";
 import Footer from "@/views/layout/Footer";
-import Sidebar from "@/views/layout/SideBar";
+import Sidebar from "@/views/layout/Sidebar";
 
 import useThemeEffect from "@/views/hooks/useThemeEffect";
 import type { FC, PropsWithChildren } from "react";
 
 const App: FC<PropsWithChildren> = ({ children }) => {
-  useThemeEffect();
-  return (
-    <div className="flex flex-col min-h-[100vh] w-full h-full">
-      <Navbar />
+    useThemeEffect();
+    return (
+        <div className="flex flex-col min-h-[100vh] w-full h-full">
+            <Navbar />
 
-      <div className="flex grow">
-        <div className="flex-shrink-0">
-          <Sidebar />
+            <div className="flex grow">
+                <div className="flex-shrink-0">
+                    <Sidebar />
+                </div>
+                <main className="grow bg-background">{children}</main>
+            </div>
+
+            <Footer />
         </div>
-        <main className="grow bg-background">{children}</main>
-      </div>
-
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default App;
--- src/views/static/ContributorList.tsx
+++ src/views/static/ContributorList.tsx
@@ -1,53 +1,40 @@
 import { useEffect, useState } from "react";
 import { fetchContributors } from "@/apis/github";
+import type { Contributor } from "@/types/contributor";
 
-type Contributor = {
-  name: string;
-  avatar: string;
-  commits: number;
-};
 
 export default function ContributorList() {
-  const [contributors, setContributors] = useState<Contributor[] | null>(null);
+    const [contributors, setContributors] = useState<Contributor[] | null>(null);
 
-  useEffect(() => {
-    fetchContributors().then(setContributors);
-  }, []);
+    useEffect(() => {
+        fetchContributors().then(setContributors);
+    }, []);
 
-  if (contributors === null) return <div>Loading contributors...</div>;
-  if (contributors.length === 0) return <div>Failed to load contributors.</div>;
+    if (contributors === null) return <div>Loading contributors...</div>;
+    if (contributors.length === 0) return <div>Failed to load contributors.</div>;
 
-  return (
-    <div className="p-5 text-gray-200">
-      <h2 className="text-2xl font-bold mb-6 ">Contributors</h2>
-      <p className="mb-6">
-        SPMods is a community-driven project. We would like to thank all the
-        contributors who have helped us make this project possible.
-      </p>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-4xl">
-        {contributors.map((c) => (
-          <div key={c.name} className="flex flex-col items-center p-6">
-            <img
-              src={c.avatar}
-              alt={c.name}
-              className="w-full h-full rounded-full mb-3"
-            />
-            <h3 className="font-semibold flex items-center">
-              <span>{c.name}</span>
-              {c.name === "moustacheManHere" && (
-                <span
-                  className="inline-block rotate-30 -translate-y-3"
-                  aria-label="ram-crown"
-                  role="img"
-                >
-                  👑
-                </span>
-              )}
-            </h3>
-            <p className="text-gray-500">{c.commits} commits</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="p-5 text-gray-200">
+            <h2 className="text-2xl font-bold mb-6 ">Contributors</h2>
+            <p className="mb-6">
+                SPMods is a community-driven project. We would like to thank all the
+                contributors who have helped us make this project possible.
+            </p>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-4xl">
+                {contributors.map((contributor) => (
+                    <div key={contributor.name} className="flex flex-col items-center p-6">
+                        <img
+                            src={contributor.avatar}
+                            alt={contributor.name}
+                            className="w-full h-full rounded-full mb-3"
+                        />
+                        <h3 className="font-semibold flex items-center">
+                            <span>{contributor.name}</span>
+                        </h3>
+                        <p className="text-gray-500">{contributor.commits} commits</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
