1110919fafbaa097d4365eb06f5b66a5c9e8f4b6
ram <justforthegreatergood@gmail.com>
2025-05-24 23:27:20 +0800

    Added function to fetch contributors

---
+++ src/apis/github.ts
@@ -0,0 +1,23 @@
+type Contributor = {
+    login: string;
+    avatar_url: string;
+    contributions: number;
+  };
+
+export async function fetchContributors() {
+    const response = await fetch('https://api.github.com/repos/spmods-official/spmods/contributors');
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status}`);
+    }
+
+    const data: Contributor[] = await response.json();
+
+    const parsed = data.map(user => ({
+        name: user.login,
+        avatar: user.avatar_url,
+        commits: user.contributions,
+    }));
+
+    return parsed;
+}
