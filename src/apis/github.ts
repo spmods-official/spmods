5c406b6234a601e1fa90772c8c03169eac9e569b
ram <justforthegreatergood@gmail.com>
2025-05-24 23:39:05 +0800

    Linted code

--- src/apis/github.ts
+++ src/apis/github.ts
@@ -1,23 +1,25 @@
 type Contributor = {
-    login: string;
-    avatar_url: string;
-    contributions: number;
-  };
+  login: string;
+  avatar_url: string;
+  contributions: number;
+};
 
 export async function fetchContributors() {
-    const response = await fetch('https://api.github.com/repos/spmods-official/spmods/contributors');
+  const response = await fetch(
+    "https://api.github.com/repos/spmods-official/spmods/contributors",
+  );
 
-    if (!response.ok) {
-        throw new Error(`Failed to fetch: ${response.status}`);
-    }
+  if (!response.ok) {
+    throw new Error(`Failed to fetch: ${response.status}`);
+  }
 
-    const data: Contributor[] = await response.json();
+  const data: Contributor[] = await response.json();
 
-    const parsed = data.map(user => ({
-        name: user.login,
-        avatar: user.avatar_url,
-        commits: user.contributions,
-    }));
+  const parsed = data.map((user) => ({
+    name: user.login,
+    avatar: user.avatar_url,
+    commits: user.contributions,
+  }));
 
-    return parsed;
+  return parsed;
 }
--- src/views/static/ContributorList.tsx
+++ src/views/static/ContributorList.tsx
@@ -20,15 +20,26 @@ export default function ContributorList() {
   return (
     <div className="p-5 text-gray-200">
       <h2 className="text-2xl font-bold mb-6 ">Contributors</h2>
-      <p className="mb-6">SPMods is a community-driven project. We would like to thank all the contributors who have helped us make this project possible.</p>
+      <p className="mb-6">
+        SPMods is a community-driven project. We would like to thank all the
+        contributors who have helped us make this project possible.
+      </p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-4xl">
         {contributors.map((c) => (
           <div key={c.name} className="flex flex-col items-center p-6">
-            <img src={c.avatar} alt={c.name} className="w-full h-full rounded-full mb-3" />
+            <img
+              src={c.avatar}
+              alt={c.name}
+              className="w-full h-full rounded-full mb-3"
+            />
             <h3 className="font-semibold flex items-center">
               <span>{c.name}</span>
               {c.name === "moustacheManHere" && (
-                <span className="inline-block rotate-30 -translate-y-3" aria-label="ram-crown" role="img">
+                <span
+                  className="inline-block rotate-30 -translate-y-3"
+                  aria-label="ram-crown"
+                  role="img"
+                >
                   👑
                 </span>
               )}
